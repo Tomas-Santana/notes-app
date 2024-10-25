9e65c0858b1bff90c3e2e78d30c61412e2805ef9
MarlonUrda <MGUR2025pro@gmail.com>
2024-10-24 23:48:33 -0400

    new animated bg

--- components/app/animatedbg.tsx
+++ components/app/animatedbg.tsx
@@ -1,73 +1,70 @@
 import React, { ReactNode, useEffect, useRef, useState } from 'react';
-import { View, StyleSheet } from 'react-native';
-import { LinearGradient } from 'expo-linear-gradient';
-import Animated, { FadeIn, FadeOut } from 'react-native-reanimated';
+import { View, StyleSheet, useWindowDimensions } from 'react-native';
+import { LinearGradient as ExpoLinearGradient } from 'expo-linear-gradient';
+import Animated, { FadeIn, FadeOut, useSharedValue, useDerivedValue, withRepeat, withTiming } from 'react-native-reanimated';
+import {
+  Canvas,
+  LinearGradient,
+  Fill,
+  interpolateColors,
+  vec,
+} from "@shopify/react-native-skia";
 
 interface AnimatedBGProps {
   children?: ReactNode
   viewStyles?: any
-  move?: boolean
 }
 
 const loc = [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1];
-let timeout: NodeJS.Timeout | undefined = undefined;
 export const colors = ['#ff65b3', '#ff59c0', '#ff4ecd', '#ff45d9', '#ff3fe6', '#f23cf2', '#d33ffe', '#ad46ff', '#7b4fff', '#0058ff', '#0060ff'];
-export const synthColors = ['#ffd319', '#ff901f', '#ff2975', '#f222ff', '#ff3fe6', '#8c1eff'];
+
+const startColors = [
+  "rgba(0, 0, 255, 1)",
+  "rgba(30, 144, 255, 1)", 
+  "rgba(70, 130, 180, 1)", 
+  "rgba(135, 206, 250, 1)",
+];
+const endColors = [
+  "rgba(138, 43, 226, 1)", 
+  "rgba(148, 0, 211, 1)", 
+  "rgba(153, 50, 204, 1)", 
+  "rgba(218, 112, 214, 1)",
+];
 
 
-export default function AnimatedBG({ children, viewStyles, move = true }: AnimatedBGProps) {
-  let [gradientOptions, setGradientOptions] = useState({
-    colors: ['#ff65b3', '#ff59c0', '#ff4ecd', '#ff45d9', '#ff3fe6', '#f23cf2', '#d33ffe', '#ad46ff', '#7b4fff', '#0058ff', '#0060ff'],
-    start: { x: 0, y: -0.2071 },
-    locations: loc,
-    end: { x: 0, y: 1 },
-  });
-
-  const movement = 0.005; // Ajuste del movimiento para una animación más suave
-
-  const gradientOptionsRef = useRef(gradientOptions);
-  gradientOptionsRef.current = gradientOptions;
-
-  let infinite = () => {
-    let newLocations = gradientOptionsRef.current.locations.map(loc => loc - movement);
-    if (newLocations[1] <= 0) {
-      newLocations = loc;
-      let gradientColors = [...gradientOptionsRef.current.colors];
-      gradientColors.push(gradientColors.shift()!);
-      setGradientOptions({
-        ...gradientOptionsRef.current,
-        colors: gradientColors,
-        locations: newLocations,
-      });
-    } else {
-      setGradientOptions({
-        ...gradientOptionsRef.current,
-        locations: newLocations,
-      });
-    }
-  };
-
+export default function AnimatedBG({ children, viewStyles }: AnimatedBGProps) {
+  const { width, height } = useWindowDimensions();
+  const colorsIndex = useSharedValue(0);
   useEffect(() => {
-    if (!move) return
-    timeout = setInterval(infinite, 50); // Ajuste del intervalo para una animación más suave
-    return () => clearInterval(timeout);
-  }, []);
-
+    colorsIndex.value = withRepeat(
+      withTiming(startColors.length - 1, {
+        duration: 4000,
+      }),
+      -1,
+      true
+    );
+  }, [colorsIndex]);
+  const gradientColors = useDerivedValue(() => {
+    return [
+      interpolateColors(colorsIndex.value, [0, 1, 2, 3], startColors),
+      interpolateColors(colorsIndex.value, [0, 1, 2, 3], endColors),
+    ];
+  }, [colorsIndex]);
   return (
-    <Animated.View style={[styles.container, viewStyles]}
-      entering={FadeIn}
-      exiting={FadeOut}
-
-    >
-      <LinearGradient
-        colors={gradientOptions.colors}
-        start={gradientOptions.start}
-        end={gradientOptions.end}
-        locations={gradientOptions.locations}
-        style={styles.gradient}
-      />
-      {children}
-    </Animated.View>
+    <View style={styles.container}>
+      <Canvas style={styles.canvas}>
+        <Fill>
+          <LinearGradient
+            start={vec(0, 0)}
+            end={vec(width, height)}
+            colors={gradientColors}
+          />
+        </Fill>
+      </Canvas>
+      <View style={styles.childrenContainer}>
+        {children}
+      </View>
+    </View>
   );
 }
 
@@ -79,7 +76,7 @@ export function StaticBG({ children, viewStyles }
       entering={FadeIn}
       exiting={FadeOut}
     >
-      <LinearGradient
+      <ExpoLinearGradient
         colors={colors}
         start={{ x: 0, y: -0.2071 }}
         end={{ x: 0, y: 1 }}
@@ -98,5 +95,13 @@ const styles = StyleSheet.create({
   gradient: {
     flex: 1,
     borderRadius: 5,
+  },
+  canvas: {
+    ...StyleSheet.absoluteFillObject,
+    zIndex: 1,
+  },
+  childrenContainer: {
+    flex: 1,
+    zIndex: 1,
   },
 });
